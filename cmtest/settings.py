14e48aa6ca305b6168210acc848d663b7b52d110
Udit <udit.wal@gmail.com>
2018-02-03 20:48:37 -0500

    Fixing DB Issue Again

--- cmtest/settings.py
+++ cmtest/settings.py
@@ -81,7 +81,12 @@ WSGI_APPLICATION = 'cmtest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
